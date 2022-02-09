9e62be103be39f002d000af31399ff19f6185ad5
Charles Thibault <charles.thibault@agencesudo.ca>
2022-02-08 20:02:10 -0500

    auctions links

--- src/nft-overrides.js
+++ src/nft-overrides.js
@@ -1,23 +1,24 @@
 export default {
-  // 'APP-13': {
-  //   tag: 'Auction',
-  //   link: {
-  //     label: 'Auction on ALGOxNFT',
-  //     href: 'https://algoxnft.com/address/APPS6P4CEXCIYI25BNDO46QSNIVVBIYR7SS5BNUZIMMTFW6GNS7UKXVAGM', 
-  //   },
-  // },
-  // 'APP-14': {
-  //   tag: 'Auction',
-  //   link: {
-  //     label: 'Auction on ALGOxNFT',
-  //     href: 'https://algoxnft.com/address/APPS6P4CEXCIYI25BNDO46QSNIVVBIYR7SS5BNUZIMMTFW6GNS7UKXVAGM', 
-  //   },
-  // },
-  // 'APP-15': {
-  //   tag: 'Giveway',
-  //   link: {
-  //     label: 'Twitter giveway',
-  //     href: 'https://twitter.com/apps_nft/status/1487810327785914372', 
-  //   },
-  // },
+  'APP-25': {
+    tag: 'Auction',
+    link: {
+      label: 'Go to auction',
+      href: 'https://algoxnft.com/address/APPS6P4CEXCIYI25BNDO46QSNIVVBIYR7SS5BNUZIMMTFW6GNS7UKXVAGM', 
+    },
+  },
+  'APP-26': {
+    tag: 'Auction',
+    link: {
+      label: 'Go to auction',
+      href: 'https://algoxnft.com/address/APPS6P4CEXCIYI25BNDO46QSNIVVBIYR7SS5BNUZIMMTFW6GNS7UKXVAGM', 
+    },
+  }, 
+  'APP-27': {
+    tag: 'Auction',
+    link: {
+      label: 'Go to auction',
+      href: 'https://algoxnft.com/address/APPS6P4CEXCIYI25BNDO46QSNIVVBIYR7SS5BNUZIMMTFW6GNS7UKXVAGM', 
+    },
+  },
+  
 }
