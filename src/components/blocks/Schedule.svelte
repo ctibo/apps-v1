80110f85a9b42875bc9cee11ff9936a55df31205
Charles Thibault <charles.thibault@agencesudo.ca>
2022-02-21 01:02:45 -0500

    added schedule

---
+++ src/components/blocks/Schedule.svelte
@@ -0,0 +1,61 @@
+<script>
+  
+</script>
+
+<style lang="scss">
+  table {
+    width: 100%;
+  }
+  td, th {
+    border-bottom: 1px solid var(--border-color);
+  }
+  th {
+    font-weight: bold;
+    text-transform: uppercase;
+    padding: 0 0.25em 1em 0;
+  }
+  td {
+    padding: 1em 0.25em 1em 0;
+    vertical-align: top;
+    font-size: clamp(0.75em, 2.5vw, 0.875em);
+  }
+</style>
+
+
+<div class="schedule">
+  <h2 class="section-title">
+    Next drops
+  </h2>
+
+  <table>
+    <tr>
+      <th>When</th>
+      <th>What</th>
+    </tr>
+
+    <tr>
+      <td>Mon. Feb 21</td>
+      <td>Mint first collab</td>
+    </tr>
+
+    <tr>
+      <td>Mon. Feb 21 to <br/>Tue. Feb 22 <br/>4PM UTC</td>
+      <td>APP #37-38-39 <br/> 24H Auction <br/>ALGOxNFT</td>
+    </tr>
+
+    <tr>
+      <td>Wed. Feb 23 to <br/>Thu. Feb 24 <br/>4PM UTC</td>
+      <td>APP Collab #1<br/> 24H Twitter Auction <br/> All proceeds to charity</td>
+    </tr>
+
+    <tr>
+      <td>Fri. Feb 25 to <br/>Sun. Feb 27 <br/>4PM UTC</td>
+      <td>APP #42<br/> Twitter art contest</td>
+    </tr>
+
+    <tr>
+      <td>Sun. Feb 27 <br/>4PM UTC</td>
+      <td>APP #40-41<br/> Regular drop <br/> Rand Gallery</td>
+    </tr>
+  </table>
+</div>
--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -2,7 +2,10 @@
   import vars from '../vars';
   import { shortenAddress } from '../helpers/address';
   import nfts from '../lib/nfts';
-  import LoadingInline from '../components/elements/LoadingInline.svelte'
+  import LoadingInline from '../components/elements/LoadingInline.svelte';
+  import Schedule from '../components/blocks/Schedule.svelte';
+  import MintPrice from '../components/blocks/MintPrice.svelte';
+  import QuickLinks from '../components/blocks/QuickLinks.svelte';
 </script>
 
 <style lang="scss">
@@ -18,97 +21,20 @@
       display: inline-block;
     }
   }
-  .algos {
-    display: inline-block;
-    width: auto;
-    font-size: 0.625em;
-    height: 1em;
-    margin-left: 0.125em;
-    vertical-align: baseline;
-  }
+  
 
-  .tiles  {
-    display: grid;
-    grid-template-columns: repeat(3, 1fr);
-    grid-gap: 1.5em;
-    margin-bottom: 1em;
-    text-align: center;
-    span, 
-    .text-link {
-      display: block;
-      font-size: clamp(0.5em, 2vw, 0.75em);
-    }
-  }
-  .tile {
-    display: block;
-    background: var(--dark-gray);
-    border-radius: 0.75em;
-    box-shadow: 0 0 2em -0.75em var(--purple);
-    transition: box-shadow 240ms ease;
-    &:hover {
-      box-shadow: 0 0 2em -0.25em var(--purple);
-    }
-    img {
-      width: 75%;
-      margin: 12.5%;
-    }
-    margin-bottom: 1em;
-  }
+  
 </style>
 
 <div class="content">
   
   <h1 class="page-title">
     About APPs
-    <span class="subtitle">
-      They're profile pics, <br/>
-      and they're awesome
-    </span>
   </h1>
 
-
-  <ul class="tiles">
-    <li>
-      <a class="tile" href="https://www.nftexplorer.app/collection?creator={vars.CREATOR_ACCOUNT}" target="_blank">
-        <img src="/images/nftexplorer-logo.png" alt="NFT Explorer" />
-      </a> 
-      <span>
-        Stats
-      </span>
-      <a class="text-link" href="https://www.nftexplorer.app/collection?creator={vars.CREATOR_ACCOUNT}" target="_blank">
-        NFT Explorer
-      </a> 
-    </li>
-
-    <li>
-      <a class="tile" href="https://www.randgallery.com/collection/apps-awesome-profile-pics" target="_blank">
-        <img src="/images/rand-logo.png" alt="Rand Gallery" />
-      </a> 
-      <span>
-        Reg drops
-      </span>
-      <a class="text-link" href="https://www.randgallery.com/collection/apps-awesome-profile-pics" target="_blank">
-        Rand Gallery
-      </a>
-    </li>
-
-    <li>
-      <a class="tile" href="https://algoxnft.com/collection/apps" target="_blank">
-        <img src="/images/algoxnft-logo.png" alt="ALGOxNFT" />
-      </a>
-      <span>
-        Auctions
-      </span>
-      <a class="text-link" href="https://algoxnft.com/collection/apps" target="_blank">
-        ALGOxNFT
-      </a>
-    </li>
-
-  </ul>
-
+  <QuickLinks />
 
   <ul class="infos">
-
     <li>
       Minted: 
       {#if $nfts && $nfts.loading}
@@ -131,38 +57,15 @@
         {shortenAddress(vars.CREATOR_ACCOUNT)}
       </a> 
     </li>
-
-    
   </ul>
 
 
   <p>
     APPs (Awesome Profile Pics) is an NFT project composed of 99 uniquely hand-drawn NFTs (1/1) on Algorand.
-    Special collabs will be added to those 99 original NFTs.
   </p>
 
-  <p>
-    NFTs will be dropped 3 at a time. 
-    The drops don't follow a predefined schedule. 
-    Drops will be announced on Twitter a couple hours upfront.
-  </p>
+  <Schedule />
 
-  <p>
-    Mint price will go as follow:
-  </p>
-  <ul class="list">
-    <li>
-      #01-24 ➞ 33<img class="algos" src="/images/algo-logo.svg" alt="Algos" />
-    </li>
-    <li>
-      #25-48 ➞ 44<img class="algos" src="/images/algo-logo.svg" alt="Algos" />
-    </li>
-    <li>
-      #49-72 ➞ 55<img class="algos" src="/images/algo-logo.svg" alt="Algos" />
-    </li>
-    <li>
-      #73-99 ➞ 66<img class="algos" src="/images/algo-logo.svg" alt="Algos" />
-    </li>
-  </ul> 
-
+  <MintPrice />
+ 
 </div>
