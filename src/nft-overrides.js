888c92f5f65355aea251342f629abf6356058e1d
Charles Thibault <charles.thibault@agencesudo.ca>
2022-03-01 16:31:13 -0500

    remove labels

--- src/nft-overrides.js
+++ src/nft-overrides.js
@@ -1,24 +1,24 @@
 export default {
-  'APP-43': {
-    tag: 'Auction',
-    link: {
-      label: 'Go to auction',
-      href: 'https://algoxnft.com/collection/apps', 
-    },
-  },
-  'APP-44': {
-    tag: 'Auction',
-    link: {
-      label: 'Go to auction',
-      href: 'https://algoxnft.com/collection/apps', 
-    },
-  }, 
-  'APP-45': {
-    tag: 'Auction',
-    link: {
-      label: 'Go to auction',
-      href: 'https://algoxnft.com/collection/apps', 
-    },
-  },
+  // 'APP-43': {
+  //   tag: 'Auction',
+  //   link: {
+  //     label: 'Go to auction',
+  //     href: 'https://algoxnft.com/collection/apps', 
+  //   },
+  // },
+  // 'APP-44': {
+  //   tag: 'Auction',
+  //   link: {
+  //     label: 'Go to auction',
+  //     href: 'https://algoxnft.com/collection/apps', 
+  //   },
+  // }, 
+  // 'APP-45': {
+  //   tag: 'Auction',
+  //   link: {
+  //     label: 'Go to auction',
+  //     href: 'https://algoxnft.com/collection/apps', 
+  //   },
+  // },
   
 }
