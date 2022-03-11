e37068f2b90c2edccea627d1364bf74ea4eaaa72
Charles Thibault <charles.thibault@agencesudo.ca>
2022-03-11 17:14:05 -0500

    remove tags

--- src/nft-overrides.js
+++ src/nft-overrides.js
@@ -20,25 +20,25 @@ export default {
   //     href: 'https://algoxnft.com/collection/apps', 
   //   },
   // },
-  'APP-52': {
-    tag: 'Shuffle',
-    link: {
-      label: 'Enter Discord Shuffle',
-      href: 'https://discord.gg/8JFAbK6ktp', 
-    },
-  },
-  'APP-53': {
-    tag: 'Shuffle',
-    link: {
-      label: 'Enter Discord Shuffle',
-      href: 'https://discord.gg/8JFAbK6ktp', 
-    },
-  }, 
-  'APP-54': {
-    tag: 'Shuffle',
-    link: {
-      label: 'Enter Discord Shuffle',
-      href: 'https://discord.gg/8JFAbK6ktp', 
-    },
-  },
+  // 'APP-52': {
+  //   tag: 'Shuffle',
+  //   link: {
+  //     label: 'Enter Discord Shuffle',
+  //     href: 'https://discord.gg/8JFAbK6ktp', 
+  //   },
+  // },
+  // 'APP-53': {
+  //   tag: 'Shuffle',
+  //   link: {
+  //     label: 'Enter Discord Shuffle',
+  //     href: 'https://discord.gg/8JFAbK6ktp', 
+  //   },
+  // }, 
+  // 'APP-54': {
+  //   tag: 'Shuffle',
+  //   link: {
+  //     label: 'Enter Discord Shuffle',
+  //     href: 'https://discord.gg/8JFAbK6ktp', 
+  //   },
+  // },
 }
