da52265f8861b5adf9362c2d2bd311e58213f466
Charles Thibault <charles.thibault@agencesudo.ca>
2022-03-18 16:18:10 -0400

    remove labels

--- src/nft-overrides.js
+++ src/nft-overrides.js
@@ -6,25 +6,25 @@ export default {
       href: 'https://discord.gg/ksZqFnA9', 
     },
   },
-  'APP-58': {
-    tag: 'Shuffle',
-    link: {
-      label: 'Enter Discord Shuffle',
-      href: 'https://discord.gg/8JFAbK6ktp', 
-    },
-  },
-  'APP-59': {
-    tag: 'Shuffle',
-    link: {
-      label: 'Enter Discord Shuffle',
-      href: 'https://discord.gg/8JFAbK6ktp', 
-    },
-  }, 
-  'APP-60': {
-    tag: 'Shuffle',
-    link: {
-      label: 'Enter Discord Shuffle',
-      href: 'https://discord.gg/8JFAbK6ktp', 
-    },
-  },
+  // 'APP-58': {
+  //   tag: 'Shuffle',
+  //   link: {
+  //     label: 'Enter Discord Shuffle',
+  //     href: 'https://discord.gg/8JFAbK6ktp', 
+  //   },
+  // },
+  // 'APP-59': {
+  //   tag: 'Shuffle',
+  //   link: {
+  //     label: 'Enter Discord Shuffle',
+  //     href: 'https://discord.gg/8JFAbK6ktp', 
+  //   },
+  // }, 
+  // 'APP-60': {
+  //   tag: 'Shuffle',
+  //   link: {
+  //     label: 'Enter Discord Shuffle',
+  //     href: 'https://discord.gg/8JFAbK6ktp', 
+  //   },
+  // },
 }
