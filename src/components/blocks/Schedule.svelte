bf1450ebb8802b547d5a0a2837cd7a3441f5c095
Charles Thibault <charles.thibault@agencesudo.ca>
2022-03-28 15:18:49 -0400

    new schedule

--- src/components/blocks/Schedule.svelte
+++ src/components/blocks/Schedule.svelte
@@ -19,13 +19,13 @@
     </tr>
 
     <tr>
-      <td>Mon Mar 21<sup>st</sup> <br/>to Tue Mar 22<sup>nd</sup> <br/>8PM UTC</td>
-      <td>APP #61-62-63 <br/> 24H Auction <br/>ALGOxNFT</td>
+      <td>Mon Mar 29<sup>th</sup> <br/>to Tue Mar 30<sup>th</sup> <br/>8PM UTC</td>
+      <td>APP #67-68-69 <br/> 24H Auction <br/>ALGOxNFT</td>
     </tr>
 
     <tr>
-      <td>Thu Mar 24<sup>th</sup> <br/>to Fri Mar 25<sup>th</sup> <br/>8PM UTC</td>
-      <td>APP #64-65-66 <br/> 24H Discord Shuffle</td>
+      <td>Thu Mar 31<sup>st</sup> <br/>to Fri Apr 1<sup>st</sup> <br/>8PM UTC</td>
+      <td>APP #70-71-72 <br/> 24H Discord Shuffle</td>
     </tr>
 
   </table>
