e453a6788689f7d5c381414f0f8b1f396266013d
Charles Thibault <charles.thibault@agencesudo.ca>
2022-04-14 15:58:23 -0400

    new mints

--- src/nft-overrides.js
+++ src/nft-overrides.js
@@ -27,4 +27,25 @@ export default {
   //     href: 'https://algoxnft.com/collection/apps', 
   //   },
   // },
+  'APP-82': {
+    tag: 'Shuffle',
+    link: {
+      label: 'Discord shuffle',
+      href: 'https://discord.gg/s2hmftgZus', 
+    },
+  },
+  'APP-83': {
+    tag: 'Shuffle',
+    link: {
+      label: 'Discord shuffle',
+      href: 'https://discord.gg/s2hmftgZus', 
+    },
+  }, 
+  'APP-84': {
+    tag: 'Shuffle',
+    link: {
+      label: 'Discord shuffle',
+      href: 'https://discord.gg/s2hmftgZus', 
+    },
+  },
 }
