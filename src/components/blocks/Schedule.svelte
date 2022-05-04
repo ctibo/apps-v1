4df454284bd1a84db3a35c3f966e910f775a07d4
Charles Thibault <charles.thibault@agencesudo.ca>
2022-05-04 16:02:16 -0400

    remove unwanted schedule

--- src/components/blocks/Schedule.svelte
+++ src/components/blocks/Schedule.svelte
@@ -18,25 +18,7 @@
       <th>What</th>
     </tr>
 
-    <tr>
-      <td>Wed Apr 27<sup>th</sup> <br/>to Thu Apr 28<sup>th</sup> <br/>8PM UTC</td>
-      <td>APP #91-92-93 <br/> 24H Auction <br/>ALGOxNFT</td>
-    </tr>
-    
-    <tr>
-      <td>Sun May 1<sup>st</sup></td>
-      <td>Prismania - Shuffle the Rainbow 
-        <br/>on ALGOxNFT
-        <br/>100% of the shuffle proceeds 
-        <br/>donated to The Trevor Project 
-      </td>
-    </tr>
-    
-    <tr>
-      <td>Mon May 2<sup>nd</sup> <br/>to Tue May 3<sup>rd</sup> <br/>8PM UTC</td>
-      <td>APP #94-95-96 <br/> 24H Discord Shuffle</td>
-    </tr>
-
+  
     <tr>
       <td>
         Thu May 5<sup>th</sup> 1AM UTC <br/>
